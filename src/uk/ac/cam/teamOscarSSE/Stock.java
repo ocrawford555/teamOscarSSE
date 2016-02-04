b1ec5b41e9bc9d7e61150a5d45d249f99fb92a5e
Akkash Kumar <Akkash Kumar>
2016-02-04 15:09:06 +0000

    Added extra fields and methods for Stock price calculation.
    Also had to change constructor for the class, set uncertainty to 0.2 as default (up to 1 as maximum, higher the value more volatile price change)

--- src/uk/ac/cam/teamOscarSSE/Stock.java
+++ src/uk/ac/cam/teamOscarSSE/Stock.java
@@ -8,6 +8,14 @@ public class Stock {
 	private long bestBid;
 	private long bestOffer;
 	private long valueEstimate;
+	
+	private long stockPrice;
+	private int TMAX;
+	private float unc;
+	
+	public int getTMAX() {
+		return TMAX;
+	}
 
 	public long getValueEstimate() {
 		return valueEstimate;
@@ -65,9 +73,25 @@ public class Stock {
 		this.stockQty -= amountToRemove;
 	}
 
-	public Stock(String sym, String nameCompany, int quantity) {
+	public Stock(String sym, String nameCompany, int quantity, int uncertainty) {
 		this.symbol = sym;
 		this.name = nameCompany;
 		this.setStockQty(quantity);
+		this.unc = uncertainty;
+	}
+	
+	//Calculates new Stock price given volume of stock traded and trading price
+	public static void newPrice(long tPrice, float volume) {
+		float tradePrice = ((float) tPrice)/100;
+		float price = ((float) stockPrice)/100;
+		float sigmaS = unc;
+		float temp = TMAX/volume -1;
+		float sigmaE = sigmaS*temp;
+		float k = (sigmaS*sigmaS)/((sigmaS*sigmaS)+(sigmaE*sigmaE));
+		price = price + (k*(tradePrice - price));
+		stockPrice = (long) price*100;
+		float temp2 = 1 - (k*sigmaS);
+		unc = (float) Math.sqrt(temp2); //New uncertainty
+		
 	}
 }
