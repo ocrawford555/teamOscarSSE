a3161632214eba0dff3bd2cda7e4239c6695b1a6
Oliver Crawford <o.crawford@hotmail.co.uk>
2016-02-09 23:23:11 +0000

    Price Moving Bot
    Slight change to functionality.

--- src/uk/ac/cam/teamOscarSSE/PriceMovingBot.java
+++ src/uk/ac/cam/teamOscarSSE/PriceMovingBot.java
@@ -28,16 +28,16 @@ public class PriceMovingBot extends Bot implements Runnable{
 
 	@Override
 	public void sendOrders() {
-		//if price are going up then buy, simple, right?
+		//if price are going up then sell, simple, right?
 		long stockP = stock.getStockPrice();
 		
-		if(stock.getPointAvg5() > stock.getPointAvg20()){
+		if(stock.getPointAvg5() < stock.getPointAvg20()){
 			int volume1 = r.nextInt(TMAX);
 			long buyPrice1 = stockP+r.nextInt(25);
 			
 			Order buyOrder1 = new BuyOrder(stock, playerID, volume1, buyPrice1);
 			
-			if(stock.getPointAvg20() > stock.getPointAvg50()){
+			if(stock.getPointAvg20() < stock.getPointAvg50()){
 				int volume2 = r.nextInt(TMAX);
 				long buyPrice2 = stockP+r.nextInt(80);
 				Order buyOrder2 = new BuyOrder(stock, playerID, volume2, buyPrice2);
@@ -47,14 +47,14 @@ public class PriceMovingBot extends Bot implements Runnable{
 			super.submitOrder(buyOrder1);
 		}
 		
-		//if price is going down, then sell, surely?
-		if(stock.getPointAvg5() < stock.getPointAvg20()){
+		//if price is going down, then buy, surely?
+		if(stock.getPointAvg5() > stock.getPointAvg20()){
 			int volume1 = r.nextInt(TMAX);
 			long sellPrice1 = stockP-r.nextInt(40);
 			
 			Order sellOrder1 = new SellOrder(stock, playerID, volume1, sellPrice1);
 			
-			if(stock.getPointAvg20() < stock.getPointAvg50()){
+			if(stock.getPointAvg20() > stock.getPointAvg50()){
 				int volume2 = r.nextInt(TMAX);
 				long sellPrice2 = stockP-r.nextInt(100);
 				Order sellOrder2 = new SellOrder(stock, playerID, volume2, sellPrice2);
