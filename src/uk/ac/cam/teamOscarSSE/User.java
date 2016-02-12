174282655afc18ddbfdc9798e209d4479f4f726e
Akkash Kumar <akkash@hotmail.co.uk>
2016-02-12 09:28:32 +0000

    Comments added

--- src/uk/ac/cam/teamOscarSSE/User.java
+++ src/uk/ac/cam/teamOscarSSE/User.java
@@ -9,23 +9,33 @@ public class User {
 	
 	public static void main(String[] args) {
 		UserFramework a = new UserFramework(s,e) {
+			
 			@Override
 			public boolean Buy(){
+				//Algorithm when to buy shares
+				
 				return false;
 			}
 			@Override
 			public int volumeToBuy() {
+				//Algorithm how many shares to buy
+				
 				return 0;
 			}
 			@Override
 			public boolean Sell(){
+				//Algorithm when to sell shares
+				
 				return false;
 			}
 			@Override
 			public int volumeToSell() {
+				//Algorithm how many shares to buy
+				
 				return 0;
 			}
 		};
+		
 		Thread user1 = new Thread(a);
 		user1.start();
 		
