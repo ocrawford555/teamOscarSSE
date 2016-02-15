8679304bddbb53c0d999bcbf80344dac2991207a
Oliver Crawford <o.crawford@hotmail.co.uk>
2016-02-15 18:30:49 +0000

    Line chart shows three rounds of play.

--- src/uk/ac/cam/teamOscarSSE/LineChart_AWT.java
+++ src/uk/ac/cam/teamOscarSSE/LineChart_AWT.java
@@ -1,5 +1,6 @@
 package uk.ac.cam.teamOscarSSE;
 
+import java.awt.Color;
 import java.util.List;
 
 import org.jfree.chart.ChartFactory;
@@ -14,14 +15,14 @@ import org.jfree.ui.RefineryUtilities;
 
 /**
  *  Display on a line graph the price of the stock
- *  over one round.
+ *  over three rounds.
  */
 public class LineChart_AWT extends ApplicationFrame
 {
 	private static final long serialVersionUID = 1L;
 	private float highest = Long.MIN_VALUE;
 	private float lowest = Long.MAX_VALUE;
-	
+
 	public LineChart_AWT( String applicationTitle , String chartTitle )
 	{
 		super(applicationTitle);
@@ -33,6 +34,7 @@ public class LineChart_AWT extends ApplicationFrame
 				true,true,false);
 
 		CategoryPlot plot = (CategoryPlot) lineChart.getPlot();
+		plot.getRenderer().setSeriesPaint(2,Color.MAGENTA);
 		ValueAxis yAxis = plot.getRangeAxis();
 		yAxis.setRange(lowest-5, highest+5);
 
@@ -45,23 +47,55 @@ public class LineChart_AWT extends ApplicationFrame
 	{
 		Main_1502_Recession.main(null);
 		DefaultCategoryDataset dataset = new DefaultCategoryDataset( );
-		
+
 		int i = 0;
-		
+
 		List<Long> graphData = Main_1502_Recession.prices;
-		
+
 		synchronized(graphData){
 			for(long value: graphData){
 				float valueNew = ((float)value) / 100;
 				if(valueNew > highest) highest = valueNew;
 				if(valueNew < lowest) lowest = valueNew;
 				i++;
-				dataset.addValue(valueNew, "BAML", String.valueOf(i));
+				dataset.addValue(valueNew, "Recession", String.valueOf(i));
+			}
+		}
+
+		Main_1502_Normal.main(null);
+
+		i = 0;
+
+		graphData = Main_1502_Normal.prices;
+
+		synchronized(graphData){
+			for(long value: graphData){
+				float valueNew = ((float)value) / 100;
+				if(valueNew > highest) highest = valueNew;
+				if(valueNew < lowest) lowest = valueNew;
+				i++;
+				dataset.addValue(valueNew, "Normal", String.valueOf(i));
+			}
+		}
+
+		Main_1502_Boom.main(null);
+
+		i = 0;
+
+		graphData = Main_1502_Boom.prices;
+
+		synchronized(graphData){
+			for(long value: graphData){
+				float valueNew = ((float)value) / 100;
+				if(valueNew > highest) highest = valueNew;
+				if(valueNew < lowest) lowest = valueNew;
+				i++;
+				dataset.addValue(valueNew, "Boom", String.valueOf(i));
 			}
 		}
 		return dataset;
 	}
-	
+
 	public static void main( String[ ] args ) 
 	{
 		LineChart_AWT chart = new LineChart_AWT(
