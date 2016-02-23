fa72e33f40ec7e65f508de184d7416c1ffa7917c
Oliver Crawford <o.crawford@hotmail.co.uk>
2016-02-23 20:46:52 +0000

    Python implementation setup - runs same algo as Java.

--- src/User.py
+++ src/User.py
@@ -1,18 +1,30 @@
-from UserFramework import runAlgo, getMaxBuy, getMaxSell
+from UserFramework import runAlgo, getMaxBuy, getMaxSell, \
+    getPrice, getOverallAvg, getBuys, getSells, \
+    getBestBuy, getBestSell, getPA, getTA, getROC
 
 def Buy():
-    return True
+    return getPA(5) > getPA(20)
 
 def Sell():
-    return True
+    return getPA(5) < getPA(20)
+
+def PriceToBuy():
+    currPrice = getBestBuy() + 1
+    return currPrice
+
+def PriceToSell():
+    currPrice = getBestSell() - 1
+    return currPrice
 
 def VolToBuy():
-    maxCanBuy = getMaxBuy()
-    return maxCanBuy/150
+    maxCanBuy = getMaxBuy()/50
+    return maxCanBuy
 
 def VolToSell():
-    maxCanSell = getMaxSell()
-    return maxCanSell/150
+    maxCanSell = getMaxSell()/50
+    return maxCanSell
 
-runAlgo('Python',Buy,Sell,VolToBuy,VolToSell)
+#runs the game
+#enter name below
+runAlgo('Python', Buy, Sell, PriceToBuy, PriceToSell, VolToBuy, VolToSell)
 
--- src/UserFramework.py
+++ src/UserFramework.py
@@ -12,6 +12,14 @@ cash = 0
 maxBuy = 0
 maxSell = 0
 
+#return top 5 from both buy and sell order book
+buys = []
+sells = []
+
+pointAvg = []
+transAvg = []
+rateOfChange = []
+
 #current_milli_time = lambda: int(round(time.time() * 1000))
 
 def getMaxBuy():
@@ -21,6 +29,61 @@ def getMaxBuy():
 def getMaxSell():
     global maxSell
     return maxSell
+
+def getPrice():
+    global stockPrice
+    return stockPrice
+
+def getOverallAvg():
+    global overallAvg
+    return overallAvg
+
+def getBuys():
+    global buys
+    return buys
+
+def getSells():
+    global sells
+    return sells
+
+def getBestBuy():
+    global buys
+    if buys!= []:
+        for k in buys:
+            for a,b in k.items():
+                return b
+    else:
+        return getPrice()
+
+def getBestSell():
+    global sells
+    if sells != []:
+        for k in sells:
+            for a,b in k.items():
+                return b
+    else:
+        return getPrice()
+
+def getPA(index):
+    global pointAvg
+    if index < pointAvg.count and pointAvg != []:
+        return pointAvg[index]
+    else:
+        return 0
+
+def getTA(index):
+    global transAvg
+    if index < transAvg.count and transAvg != []:
+        return transAvg[index]
+    else:
+        return 0
+
+def getROC(index):
+    global rateOfChange
+    if index < rateOfChange.count and rateOfChange != []:
+        return rateOfChange[index]
+    else:
+        return 0
 
 def registerUser(name, urlA):
     global token
@@ -48,7 +111,8 @@ def submitSell(urlA,volume,price,uToken):
     resp = requests.post(urlA,json=payload)
 
 def update(urlA,uToken):
-    global stockPrice,stock,overallAvg
+    global stockPrice,stock,overallAvg,\
+        pointAvg,transAvg,rateOfChange
     getCash(urlA,uToken)
     urlA = urlA + 'stock/' + stock
     payload = {'user-token' : uToken}
@@ -60,7 +124,12 @@ def update(urlA,uToken):
             stockPrice = v
         elif x == 'overallAvg':
             overallAvg = v
-
+        elif x == 'pointAvg':
+            pointAvg = v
+        elif x == 'transactionAvg':
+            transAvg = v
+        elif x == 'rateOfChange':
+            rateOfChange = v
 
 def getCash(urlA,uToken):
     global cash,maxBuy,maxSell
@@ -78,6 +147,7 @@ def getCash(urlA,uToken):
             maxSell = v
 
 def obtainOrderBook(urlA):
+    global buys,sells
     urlA += 'orderbook/BAML'
 
     try:
@@ -86,15 +156,15 @@ def obtainOrderBook(urlA):
 
         for x,v in json_data.items():
             if x == 'buy':
-                print(v)
+                buys = v[0:5]
             elif x == 'sell':
-                print(v)
+                sells = v[0:5]
 
     except ConnectionError:
         print('Error obtaining order book, sorry.')
 
 
-def runAlgo(name,Buy,Sell,VolToBuy,VolToSell):
+def runAlgo(name,Buy,Sell,PriceToBuy,PriceToSell,VolToBuy,VolToSell):
     registerUser(name, url)
 
     #timeStart = current_milli_time()
@@ -106,9 +176,9 @@ def runAlgo(name,Buy,Sell,VolToBuy,VolToSell):
             #parameters in order
             #url, quantity, price, token from registration
             if Buy() == True:
-                submitBuy(url,VolToBuy(),stockPrice+1,token)
+                submitBuy(url,VolToBuy(),PriceToBuy(),token)
 
             if Sell() == True:
-                submitSell(url,VolToSell(),stockPrice-1,token)
+                submitSell(url,VolToSell(),PriceToSell(),token)
 
-            #obtainOrderBook(url)
+            obtainOrderBook(url)
